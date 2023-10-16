3510afa330ba13d05417c1ce2096bd5e2fc9f601
acme <acme@acmecia.com>
2023-10-16 15:07:40 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@
 import './App.css';
 
 
-// import { Test } from './components/test';
-
-// import * as utils from './components/utils';
 import { Fragment } from 'react';
 
-// import { useState } from 'react';
 
-// import TicTacToe from './components/tictactoe';
-// import MyAnimation from './components/animation';
 import Draw from './components/image1';
 
 
@@ -20,25 +14,7 @@ export function App() {
     // tmb se podría usar algo como un div, pero tiene implicación visual
     // se puede usar <> </> pero no es compatible con todos los navegadores 
     <Fragment> 
-      {/* <Test /> */}
-      {/* <utils.MyButton />
-      <utils.MyButtonCounter count={count} onClick={handleClick} />
-      <utils.MyButtonCounter count={count} onClick={handleClick} />
-      <h1> {utils.user.name} </h1>
-      <img
-        className="avatar"
-        src={utils.user.imageUrl}
-        alt={'Photo of ' + utils.user.name}
-        style={{
-          width: utils.user.imageSize,
-          height: utils.user.imageSize
-        }}
-      />
-      <utils.ShoppingList/>
-      <TicTacToe />
-      <TicTacToe />
-      <MyAnimation /> */}
-
+  
       <Draw />
     
     </Fragment>
